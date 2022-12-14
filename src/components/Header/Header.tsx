8be1f7f5d7fc74b1a3188450f85ef4b79d405876
Kacper Żarna <kacper@192.168.1.16>
2022-12-14 20:34:39 +0100

    Kod 14.12.2022 20:35

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>ZDFRONpol18 Clicker</h1>
+      <h2>Pierwsza aplikacja React'owa</h2>
+      <span>To jest header</span>
+    </div>
+  );
+};
+
+export default Header;
