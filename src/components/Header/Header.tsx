2382fa75a58845b154b1433c9fee21c90522ffa9
Kacper Żarna <kacper@192.168.1.11>
2023-01-10 21:01:10 +0100

    Kod 21:00 10.01.2023

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css";
+
 const OurCustomHeader = () => {
   return (
     <>
       <h1>ZDFRONpol18 Clicker</h1>
       <h2>Pierwsza aplikacja React'owa</h2>
-      <span>To jest header</span>
+      <span className={classes.welcome}>To jest header</span>
     </>
   );
 };
